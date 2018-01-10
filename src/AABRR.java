1304018206c488449cb5369bb78c9a4a254e54e4
Bowmii <motard.bastien@gmail.com>
2018-01-10 17:26:10 +0100

    Insertion partielle

--- src/AABRR.java
+++ src/AABRR.java
@@ -1,51 +1,65 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.FileWriter;
-
-public class AABRR extends Arbre {
-	private int min; //minimum
-	private int Max; // Maximum
+import java.io.*;
+public class AABRR {
+	private Integer min; //minimum
+	private Integer Max; // Maximum
 	private ABRR AA;
 	private AABRR SAG;
 	private AABRR SAD;
 	/**
 	 * @param args
 	 */
-	public AABRR(int m, int M, int racine){
+	public AABRR(Integer m, Integer M, Integer racine){
 		super();
 		this.min = m;
 		this.Max = M;
 		this.AA = new ABRR(racine);
-
-	}
-	
-	public void CreerSAG(int m, int M, int racine){
-		this.SAG = new AABRR(m,M,racine);
-	}
-	
-	public void CreerSAD(int m, int M, int racine){
-		this.SAD = new AABRR(m,M,racine);
 	}
 	
 	public ABRR getAA(){
 		return this.AA;
 	}
 	
+	public Integer getmin() {
+		return this.min;
+	}
+	
+	public Integer getMax() {
+		return this.Max;
+	}
+	
+	public void CreerSAG(Integer min, Integer max, Integer rac)
+	{
+		this.SAG = new AABRR(min, max, rac);
+	}
+	
+	public void CreerSAD(Integer min, Integer max, Integer rac)
+	{
+		this.SAD = new AABRR(min, max, rac);
+	}
+	
+	public AABRR getSAG()
+	{
+		return this.SAG;
+	}
+	
+	public AABRR getSAD()
+	{
+		return this.SAD;
+	}
+	
 	public String Parcours(String valeur){
 		valeur = valeur + this.min + ':' +  this.Max + ';' ;
 		valeur = AA.Parcours(valeur);
-		valeur = valeur + System.getProperty("line.separator");
+			valeur = valeur + System.getProperty("line.separator");
 		if (this.SAG != null)
 		{
-		valeur = this.SAG.Parcours(valeur);
+			valeur = this.SAG.Parcours(valeur);
 		}
-		if (this.SAD != null) 
+		if (this.SAD != null)
 		{
-		valeur = this.SAD.Parcours(valeur);
+			valeur = this.SAD.Parcours(valeur);
 		}
-		//super.Parcours(valeur);
-		return valeur;
+				return valeur;
 	}
 	
 	public void Save(String nomFichier, String chemin)
@@ -63,7 +77,7 @@ public class AABRR extends Arbre {
 		}
 		try
 		{
-		java.io.FileOutputStream Flux = new java.io.FileOutputStream(nomFichier); // Doit être utilisé dans un bloc TRY
+		java.io.FileOutputStream Flux = new java.io.FileOutputStream(nomFichier); 
 		FileWriter fw = new FileWriter(nomFichier);
 		Parcours = this.Parcours(Parcours);
 		fw.write(Parcours);
@@ -79,24 +93,119 @@ public class AABRR extends Arbre {
 			System.out.println("Impossible d'écrire dans le fichier");
 			System.out.println(err3);
 		}
-
-		System.out.println(Parcours);
 	    File source = new File(nomFichier);
 	    chemin = chemin + "/" + nomFichier;
 	    File destination = new File(chemin);
 	    source.renameTo(destination);
 	}
 	
-	public static void main(String[] args) {
-		AABRR GrandArbre = new AABRR(50 ,75, 60);
-		String val = "";
-		GrandArbre.getAA().CreerSAD(55);
-		GrandArbre.CreerSAD(78, 80, 80);
-		GrandArbre.CreerSAG(9, 22, 9);
-		GrandArbre.Save("Parcours.txt", "/home/bastien/eclipse-workspace/Arbre/src");
+	public void insertAABRR(AABRR ArbreTest)
+	{
+		if (ArbreTest.getMax() <= this.getmin())
+		{
+			if (this.SAG == null)
+			{
+				CreerSAG(ArbreTest.getmin(), ArbreTest.getMax(), ArbreTest.getAA().getRacine());
+				
+			}else {
+				this.getSAG().insertAABRR(ArbreTest);
+			}
+		}else 
+		{
+			if (this.SAD == null)
+			{
+				CreerSAD(ArbreTest.getmin(), ArbreTest.getMax(), ArbreTest.getAA().getRacine());
+			}else {
+				this.getSAD().insertAABRR(ArbreTest);
+			}
+		}
+	}
+	
+	public AABRR(String chemin)
+	{
+		AABRR Arbre1 = new AABRR (null, null, null);
+		try
+		{
+			InputStream ips=new FileInputStream(chemin); 
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String ligne, val="";
+			while((ligne=br.readLine()) != null)
+			{
+				if (this.min == null && this.Max == null && this.getAA() == null)
+				{
+					this.CreaLigne(ligne); 
+				}
+				else {
+					Arbre1.CreaLigne(ligne);		
+					this.insertAABRR(Arbre1);
+				}
+			}
+			br.close(); 
+		}
 		
-		//val = GrandArbre.Save();
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+	}
 
+
+	
+		
+	public void CreaLigne(String ligne) 
+	{
+		boolean sepmM = false, sepAa = false, finpremVal = false;
+		String RacMin = "", RacMax = "", premVal = "", valSuivante = "", val = "";
+		if (ligne != null)
+			{
+				for (Integer ii = 0; ii < ligne.length(); ++ii)
+				{
+					if (sepmM == false && ligne.charAt(ii) != ':' && sepAa == false)
+					{
+						RacMin = RacMin + ligne.charAt(ii);
+					}					
+					if (sepmM == true && ligne.charAt(ii) != ';' && sepAa == false)
+					{
+						RacMax = RacMax + ligne.charAt(ii);						
+					}
+					if (sepmM == true && sepAa == true && finpremVal == false && ligne.charAt(ii) != ':')
+					{
+						premVal = premVal + ligne.charAt(ii);
+					}
+					if(finpremVal == true && ligne.charAt(ii) != ':' )
+					{
+						valSuivante = valSuivante + ligne.charAt(ii);
+					}
+					if (sepmM == false && ligne.charAt(ii) == ':' && sepAa == false)
+					{
+						sepmM = true;
+						this.min =  Integer.parseInt(RacMin);
+					}
+					if (sepmM == true && ligne.charAt(ii) == ';' && sepAa == false)
+					{
+						sepAa = true;
+						this.Max =  Integer.parseInt(RacMax);
+					}
+					if(finpremVal == true && (ligne.charAt(ii) == ':' || ligne.charAt(ii) == '\0' || ii == ligne.length()-1 ))
+					{
+						this.getAA().insertABRR((Integer.parseInt(valSuivante)));
+						valSuivante = "";
+					}
+					if (sepmM == true && sepAa == true && finpremVal == false && (ligne.charAt(ii) == ':' || ligne.charAt(ii) == '\0' || ii == ligne.length()-1 ) )
+					{
+						finpremVal = true;
+						this.AA = new ABRR( Integer.parseInt(premVal));
+					}
+				}
+		}
+	}
+	
+	public static void main(String[] args) {
+		AABRR GrandArbre = new AABRR("/home/bastien/eclipse-workspace/Arbre/src/coucou.txt");
+		String val = "";
+		GrandArbre.Save("Parcours.txt", "/home/bastien/eclipse-workspace/Arbre/src");
+		val = GrandArbre.Parcours(val);
+		System.out.println(val);
 	}
 
 }
--- src/ABRR.java
+++ src/ABRR.java
@@ -1,53 +1,95 @@
-public class ABRR extends Arbre{
-	private int racine;
+public class ABRR{
+	private Integer racine;
 	private ABRR SAG;
-	private ABRR SAD; 
+	private ABRR SAD;
 	/**
 	 * @param args
 	 */
-	public ABRR(int racine){
+	public ABRR(Integer racine){
 		this.racine = racine;
 		this.SAG = null;
 		this.SAD = null;		
 	}
-	
-	public void CreerSAG(int racine){
-		this.SAG = new ABRR(racine);
+	public void CreerSAG(Integer rac)
+	{
+		this.SAG = new ABRR(rac);
 	}
 	
-	public void CreerSAD(int racine){
-		this.SAD = new ABRR(racine);
+	public void CreerSAD(Integer rac)
+	{
+		this.SAD = new ABRR(rac);
 	}
 	
-	public void choixSA(int racineSA){
+	public ABRR getSAG()
+	{
+		return this.SAG;
+	}
+	
+	public ABRR getSAD()
+	{
+		return this.SAD;
+	}
+	
+	public Integer getRacine()
+	{
+		return this.racine;
+	}
+		
+	public void choixSA(Integer racineSA){
 		if (racineSA <= this.racine){
 			CreerSAD(racineSA);
 		}else{
 			CreerSAG(racineSA);
 		}
 	}
-	
-	public String Parcours(String valeur){
-		if (this.SAG == null && this.SAD == null) 
+		
+	public String Parcours(String valeur){ 
+		valeur = valeur + this.racine;
+		if (this.SAG != null || this.SAD != null)
 		{
-			valeur = valeur + this.racine;
+			valeur = valeur + ':';
 		}
-		else {
-			
-			valeur = valeur + this.racine + ':';
-			if (this.SAG != null) 
-			{
-				valeur = this.SAG.Parcours(valeur);
-			}
-			if (this.SAD != null) 
-			{
-				valeur = this.SAD.Parcours(valeur);
-			}
+		if (this.SAG != null) 
+		{
+			valeur = this.SAG.Parcours(valeur);
 		}
-		//}
-		//super.Parcours(valeur);
+
+		if (this.SAD != null) 
+		{
+			valeur = valeur + ':';
+			valeur = this.SAD.Parcours(valeur);
+		}
 		return valeur;
 	}
+	
+	public void insertABRR(Integer valeur)
+	{
+		if(this.racine == null)
+		{
+			this.racine = valeur;
+		} 
+		else {			
+			if (valeur >= this.racine)
+			{
+				if (this.SAG == null)
+				{
+					CreerSAG(valeur);
+				}else {
+					this.getSAG().insertABRR(valeur);
+				}
+			}
+			else{
+				if (this.SAD == null)
+				{
+					CreerSAD(valeur);
+				}else {
+					this.getSAD().insertABRR(valeur);
+				}
+			}	
+		}
+	}
+	
+	
 	
 		
 	public static void main(String[] args) {
